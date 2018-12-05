c271eef8ff6cb1ca500c23ce19b2fa661b94cb30
BaltShip <obmens1c@mail.ru>
2018-12-05 10:27:12 +0300

    New

---
+++ src/test/java/TestTask.java
@@ -0,0 +1,9 @@
+import module.TaskHelpDesk;
+
+public class TestTask {
+    public static void main(String[] args) {
+        TaskHelpDesk taskHelpDesk = new TaskHelpDesk();
+        taskHelpDesk.setName("Uriy");
+        System.out.println(taskHelpDesk.getName());
+    }
+}

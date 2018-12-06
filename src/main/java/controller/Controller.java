bce04968aaf83a9a6c92d39ec4c398cfa90da477
obmens1C <obmens1C@mail.ru>
2018-12-06 17:17:09 +0300

    User class and JavaFX

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -3,16 +3,37 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class Controller {
 
     @FXML
-    public Button Exit;
+    public Button buttonExit;
+    @FXML
+    public Button buttonNewUser;
 
     @FXML
-    public void buttonExitMainScene(ActionEvent actionEvent) {
+    public void onActionButtonExitMainScene(ActionEvent actionEvent) {
         System.exit(0);
+    }
+
+    @FXML
+    public void onActionButtonNewUser(ActionEvent actionEvent) {
+        try {
+            String fxmlFile = "/fxml/user.fxml";
+            FXMLLoader loader = new FXMLLoader();
+            Parent root = loader.load(getClass().getResourceAsStream(fxmlFile));
+            Stage stage = new Stage();
+            stage.setTitle("Task manager Help Desk");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }

c64f679dc1af26f75f14b9f43bd75389495c64ca
obmens1C <obmens1C@mail.ru>
2018-12-06 15:36:25 +0300

    User class is prefinish

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,11 +1,12 @@
 
-import model.Department;
+import exception.PolicyPasswordException;
 import model.User;
 
 public class Test {
-    public static void main(String[] args) {
-        User Dima = new User(1, "Dima");
+    public static void main(String[] args) throws PolicyPasswordException {
+        char[] password = {'1', '2', 't', 'S', '5', '6', '7', 'a'};
+        User Dima = new User(1, "Dima", "Ivanov", password);
 
-        System.out.println(Dima.getId() + Dima.getName() + Dima.getDepartment());
+        System.out.println(Dima.getId() + " " + Dima.getName() + " " + Dima.getSurname() + " " + Dima.getDepartment());
     }
 }
